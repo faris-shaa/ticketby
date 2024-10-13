aae6056e737a45e74c12cdc386b6bcc648200168
faris <faris.shaaruny@gmail.com>
2024-10-13 18:23:25 +0000

    event pages fixed my ticket fixed

--- resources/views/front/events.blade.php
+++ resources/views/front/events.blade.php
@@ -121,9 +121,8 @@ $lang = session('direction') == 'rtl' ? 'ar' : 'en';
                 $(this).css('width', '3.5rem');
             });
         });
-        var SearchEventCat = $("#category_id_input").val();
-        var SearchEventCity = $("#city_id_input").val();
-        let limit = 6;
+
+        let limit = -1;
         $("#load_more").click(function() {
             limit += 3
             fetchEvents(SearchEventName, SearchEventDate, SearchEventCity, SearchEventCat, limit);
@@ -146,13 +145,7 @@ $lang = session('direction') == 'rtl' ? 'ar' : 'en';
             fetchEvents(SearchEventName, SearchEventDate, SearchEventCity, SearchEventCat, limit);
 
         })
-        $("#clear_search").on("click", function() {
-            $("#clear_search").addClass('hidden');
-            $("#Searchbtn").addClass('hidden');
-            $('#SearchEventName').val('')
-            fetchEvents('', SearchEventDate, SearchEventCity, SearchEventCat, limit);
-        })
-
+        var SearchEventCity = ''
         $('#SearchEventCity').on('change', function() {
             SearchEventCity = $(this).val();
             console.log(SearchEventCity);
@@ -165,7 +158,7 @@ $lang = session('direction') == 'rtl' ? 'ar' : 'en';
             fetchEvents(SearchEventName, SearchEventDate, SearchEventCity, SearchEventCat, limit);
 
         });
-
+        var SearchEventCat = ''
         $('#SearchEventCat').on('change', function() {
             SearchEventCat = $(this).val();
             fetchEvents(SearchEventName, SearchEventDate, SearchEventCity, SearchEventCat, limit);
